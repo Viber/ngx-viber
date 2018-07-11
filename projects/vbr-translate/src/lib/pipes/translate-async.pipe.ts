16faeb32f2d23861485aec241c0038dae0641d92
Peter Pshenichny <peter@viber.com>
2018-07-11 15:57:47 +0300

    VbrTranslateAsyncPipe is not require "| async" pipe anymore, it become not pure and can handle params as Observable, or regular. params can be changed from Observable to regular as well.

--- projects/vbr-translate/src/lib/pipes/translate-async.pipe.ts
+++ projects/vbr-translate/src/lib/pipes/translate-async.pipe.ts
@@ -1,21 +1,46 @@
-import { ChangeDetectorRef, Pipe, PipeTransform } from '@angular/core';
-import { TranslatePipe } from '@ngx-translate/core';
-import { TranslateService } from '@ngx-translate/core';
-import { combineLatest, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { OnDestroy, Pipe, PipeTransform } from '@angular/core';
+import { TranslatePipe, } from '@ngx-translate/core';
+import { BehaviorSubject, combineLatest, Observable, of, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-@Pipe({name: 'vbrTranslateAsync'})
-export class VbrTranslateAsyncPipe implements PipeTransform {
-  private transPipe: TranslatePipe;
+@Pipe({
+  name: 'vbrTranslateAsync',
+  pure: false
+})
+export class VbrTranslateAsyncPipe extends TranslatePipe implements PipeTransform, OnDestroy {
+  private onDestroy$: Subject<any> = new Subject();
+  protected lastParams$: Observable<Object> | Object;
+  protected lastQuery$: BehaviorSubject = new BehaviorSubject(undefined);
 
-  constructor(private translate: TranslateService, _ref: ChangeDetectorRef) {
-    this.transPipe = new TranslatePipe(translate, _ref);
+  transform(query: string, params$?: any): any {
+    // In case params$ changed
+    if (params$ !== this.lastParams$) {
+      this.lastParams$ = params$;
+      // Init destroy of old observable
+      this.onDestroy$.next();
+
+      // In case params$ is not observable, wrap it
+      if (!(params$ instanceof Observable)) {
+        params$ = of(params$);
+      }
+
+      // Create new Subscription to changes from both query and parameters changes.
+      // It is responsible to emit new pairs of query and parameter to parent translate method.
+      combineLatest(params$, this.lastQuery$)
+        .pipe(takeUntil(this.onDestroy$))
+        .subscribe(([p, q]) => super.transform(q, p));
+    }
+
+    // Query string changed, emit the change
+    if (query !== this.lastQuery$.value) {
+      this.lastQuery$.next(query);
+    }
+
+    return this.value;
   }
 
-  transform(query: string, values$: Observable<{}>): any {
-    return combineLatest(this.translate.onLangChange, values$)
-      .pipe(
-        map(([lang, values]) => this.transPipe.transform(query, values, Math.random()))
-      );
+  ngOnDestroy() {
+    super.ngOnDestroy();
+    this.onDestroy$.next();
   }
 }
