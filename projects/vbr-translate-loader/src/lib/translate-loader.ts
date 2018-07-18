dc2bc7537c28f77b81ac97e272f9cebb9242f7e8
Peter Pshenichny <peter@viber.com>
2018-07-18 15:03:26 +0300

    Added silentException Removed dependency on VbrTranslateModule Added Tests

--- projects/vbr-translate-loader/src/lib/translate-loader.ts
+++ projects/vbr-translate-loader/src/lib/translate-loader.ts
@@ -1,39 +1,30 @@
-import { Observable, of, zip } from 'rxjs';
+import { Observable, of, throwError, zip } from 'rxjs';
 import { TranslateLoader } from '@ngx-translate/core';
-import { Optional } from '@angular/core';
-import { VbrTranslateService } from '@viberlab/translate';
 import { catchError, map, tap } from 'rxjs/operators';
 
 export class VbrTranslateLoader implements TranslateLoader {
   protected loaders: Array<TranslateLoader> = [];
 
-  constructor(additionalLoaders: Array<TranslateLoader>, @Optional() private  vbrTranslateService?: VbrTranslateService) {
+  constructor(
+    additionalLoaders: Array<TranslateLoader>,
+    protected silentExceptions: boolean = false,
+  ) {
     additionalLoaders.map(loader => this.addLoader(loader));
   }
 
   public getTranslation(lang: string): Observable<any> {
-    if (this.vbrTranslateService) {
-      // Started loading
-      this.vbrTranslateService.loadingEvent.next(true);
-    }
     const observables: Array<Observable<any>> = this.loaders.map(loader => {
       return loader.getTranslation(lang)
         .pipe(
           catchError(e => {
-            return of({});
+            return this.silentExceptions ? of({}) : throwError(e);
           })
-        )
-        ;
+        );
     });
 
     return zip(...observables)
       .pipe(
-        map(loaders => Object.assign({}, ...loaders)),
-        tap(() => {
-          if (this.vbrTranslateService) {
-            this.vbrTranslateService.loadingEvent.next(false);
-          }
-        })
+        map(loaders => Object.assign({}, ...loaders))
       );
   }
 
