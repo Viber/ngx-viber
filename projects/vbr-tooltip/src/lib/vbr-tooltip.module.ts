134fa1e15fe89f87cc2d9a509989a6884c6673fa
Pini Bibergal <pini@viber.com>
2018-08-05 11:12:19 +0300

    removed browser module from tooltip

--- projects/vbr-tooltip/src/lib/vbr-tooltip.module.ts
+++ projects/vbr-tooltip/src/lib/vbr-tooltip.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { VbrTooltipComponent } from './vbr-tooltip.component';
 import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule],
   declarations: [VbrTooltipComponent],
   exports: [VbrTooltipComponent]
 })
